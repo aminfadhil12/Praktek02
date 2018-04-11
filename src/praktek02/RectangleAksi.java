0ee7e5e7061edea041b20b6c2180adf9cab4ebce
aminfadhil12 <aminfadhil.fadhil268@gmail.com>
2018-04-11 12:23:29 +0800

    Mengisi nilai atribute

--- src/praktek02/RectangleAksi.java
+++ src/praktek02/RectangleAksi.java
@@ -3,6 +3,9 @@ public class RectangleAksi {
     public static void main(String[] args) {
         
         Rectangle r1  = new Rectangle();
+        
+        r1.panjang = 10;
+        r1.lebar = 20;
     }
 
 }
